9fe0cf481abb723bb7e9e316dfbb0ab3123cbc02
tekalu <hikaru_t@mac.com>
2025-01-08 00:59:04 +0900

    結果表示の修正

--- components/atoms/api/result-card.vue
+++ components/atoms/api/result-card.vue
@@ -47,8 +47,8 @@
         <template v-slot:modal>
 
           <div class="flex flex-col items-start justify-center overflow-auto relative w-[90vw] max-h-[90vh] text-xs">
-            <pre v-if="executionResult.success" class="bg-gray-50 p-4 rounded ">{{ formatResponse(executionResult.data) }}</pre>
-            <pre v-if="!executionResult.success" class="bg-gray-50 p-4 rounded ">{{ formatResponse(executionResult.error) }}</pre>
+            <pre v-if="executionResult.success" class="bg-gray-50 p-4 rounded w-full">{{ formatResponse(executionResult.data) }}</pre>
+            <pre v-if="!executionResult.success" class="bg-gray-50 p-4 rounded w-full">{{ formatResponse(executionResult.error) }}</pre>
           </div>
         </template>
       </AtomsCommonModalWindow>
